956bdf732801ac64ea73cdfb452a108487fd40c1
bm20894 <milesboz@me.com>
2018-12-25 16:03:31 -0600

    added .make.sh

---
+++ .make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script creates sym links in home directory for dotfiles
+
+dir=~/dotfiles
+old_dir=~/dotfiles_old
+files=".bashrc .zshrc .tmux.conf .vimrc"
+
+echo "Creating $old_dir backup for existing dotfiles in ~"
+mkdir -p $old_dir
+echo "...Complete"
+
+cd $dir
+
+echo "Moving $file from ~ to $old_dir"
+for file in $files; do
+	mv ~/$file $old_dir
+	echo "Creating symlink to $file in ~"
+	ln -s $dir/$file ~/$file
+	echo "...Complete"
+done
